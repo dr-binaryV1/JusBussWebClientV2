b34ff37845bcac8e505cf3af047eb73cacab8045
dr-binaryV1 <developerdamian@gmail.com>
2017-10-14 12:49:21 -0500

    add header and signup component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Header from './components/common/header';
+import SignUp from './components/auth/signup';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <Header />
+    <SignUp />
+  </div>
+, document.getElementById('root'));
 registerServiceWorker();
